a91568a577ad94a870d31cb47371c47d0c4421b3
Paul Giura <giurapaul74@gmail.com>
2021-09-01 21:25:30 +0300

    A simple calculator app

---
+++ Source/Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Source/Calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            
+
+            while (true)
+            {
+                //Console.WriteLine("Which operation would you like to do?");
+                Console.WriteLine("Press 1 for Addition.");
+                Console.WriteLine("Press 2 for Subtraction.");
+                Console.WriteLine("Press 3 for Multiplication.");
+                Console.WriteLine("Press 4 for Divide.");
+                Console.WriteLine("Press 5 to quit.");
+
+                var option = Console.ReadLine();
+
+                if (option == "5")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Enter first input: ");
+                var input1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second input: ");
+                var input2 = int.Parse(Console.ReadLine());
+                var result = 0;
+
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("Addition.");
+                        result = calculator.Addition(input1, input2);
+                        break;
+                    case "2":
+                        Console.WriteLine("Subtraction.");
+                        result = calculator.Subtraction(input1, input2);
+                        break;
+                    case "3":
+                        Console.WriteLine("Multiplication.");
+                        result = calculator.Multiplication(input1, input2);
+                        break;
+                    case "4":
+                        Console.WriteLine("Division.");
+                        result = calculator.Divide(input1, input2);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input. Try again.");
+                        break;
+                }
+
+                Console.WriteLine($"Your result is {result}");
+                Console.WriteLine();
+                Console.WriteLine("Choose your operation.");
+            }
+
+        }
+    }
+}
